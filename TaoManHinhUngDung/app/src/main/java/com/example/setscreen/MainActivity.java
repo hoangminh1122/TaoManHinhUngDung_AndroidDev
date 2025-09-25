03d514a924ba8d75ecf8b07a6b53f758310f8ddc
ADMIN-PC\Admin <nminhhoang769@gmail.com>
2025-09-25 23:47:09 +0700

    no message

--- TaoManHinhUngDung/app/src/main/java/com/example/setscreen/MainActivity.java
+++ TaoManHinhUngDung/app/src/main/java/com/example/setscreen/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.setscreen;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1. Khai báo biến cho nút bấm
+    Button buttonGoToCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // 2. Ánh xạ (kết nối) biến với nút bấm trong XML qua ID
+        buttonGoToCreateAccount = findViewById(R.id.buttonGoToCreateAccount);
+
+        // 3. Gán sự kiện click cho nút
+        buttonGoToCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Khi nút được nhấn, tạo một Intent để mở màn hình CreateAccountActivity
+                Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
